d2b40be2644f01ea2ff3f660895a1ef9e0e3209b
Naveen B <naveenbnaveen2898@gmail.com>
2025-03-12 21:34:43 +0530

    added m1 method

--- src/main/java/basic/Helloworld.java
+++ src/main/java/basic/Helloworld.java
@@ -4,6 +4,12 @@ public class Helloworld {
 
 	public static void main(String[] args) {
 		System.out.println("hello naveen");
+	
 	}
 
+//side branch demo	
+public void m1() {
+	System.out.println("from b1 branch");
+	
+}
 }
